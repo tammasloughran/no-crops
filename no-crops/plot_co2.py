2961be8aed9197a5416f0acb3b78912ce9a1b4be
tammasloughran <tammas.loughran@csiro.au>
2023-07-19 10:49:24 +1000

    Changed linestyle in co2 plot

--- no-crops/plot_co2.py
+++ no-crops/plot_co2.py
@@ -19,6 +19,7 @@ EXPERIMENTS = [
         'GWL-NoCrops-B2050',
         'GWL-NoCrops-B2055',
         'GWL-NoCrops-B2060',
+        'GWL-EqFor-B2060',
         ]
 COLORS = {
         'PI-GWL-t6':'#62EA00',
@@ -35,6 +36,7 @@ COLORS = {
         'GWL-NoCrops-B2050':'#055992',
         'GWL-NoCrops-B2055':'#1140AB',
         'GWL-NoCrops-B2060':'#1E31B6',
+        'GWL-EqFor-B2060','lightblue'
         }
 YEARS = {
         'PI-GWL-t6':np.arange(0*12, 700*12)/12,
@@ -51,6 +53,7 @@ YEARS = {
         'GWL-NoCrops-B2050':np.arange(400*12, 502*12)/12,
         'GWL-NoCrops-B2055':np.arange(400*12, 502*12)/12,
         'GWL-NoCrops-B2060':np.arange(400*12, 502*12)/12,
+        'GWL-EqFor-B2060':np.arange(400*12, 502*12)/12,
         }
 
 all_data = dict()
@@ -66,9 +69,9 @@ for experiment in EXPERIMENTS:
     np.save(f'data/co2_{experiment}_all.npy', all_data[experiment])
 
     if 'NoCrops' in experiment:
-        lstyle = 'dotted'
-    else:
         lstyle = 'solid'
+    else:
+        lstyle = 'dotted'
     handle = plt.plot(
             YEARS[experiment],
             all_data[experiment],
@@ -83,6 +86,6 @@ plt.legend(handles, labels, frameon=False)
 plt.xlabel('Time (years)')
 plt.ylabel('CO$_2$ (ppm)')
 plt.xlim(left=0, right=700)
-plt.savefig('co2_global_warming_level_no_crops.svg', dpi=200)
+plt.savefig('plots/co2_global_warming_level_no_crops.svg', dpi=200)
 plt.show()
 
